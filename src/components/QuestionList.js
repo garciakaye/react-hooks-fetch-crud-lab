21b7c599fd07fe84c95f840241b248e4fdb1c311
kaye garcia <kayemgarcia@gmail.com>
2021-10-18 16:10:48 -0500

    In progress

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect, useState} from "react";
+import QuestionItem from "./QuestionItem"
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+    .then((r) => r.json())
+    .then((questions) => setQuestions(questions))
+  }, []);
+
+  const questionsToDisplay = questions.filter((question) => {
+    return question.prompt 
+  })
+
+  console.log(questionsToDisplay)
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {/* display QuestionItem components here after fetching */}
+        </ul>
     </section>
   );
 }
